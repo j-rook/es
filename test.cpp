72b8fc3e8f4c0174d4d1b825351f367aa70ccb45
Andrew Lesh <j.rook@zoho.com>
2017-08-06 13:33:04 -0400

    began implementing file io for ease

--- test.cpp
+++ test.cpp
@@ -1,4 +1,10 @@
 #include "test.h"
+#include <fstream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::fstream;
 
 int main()
 {
@@ -35,13 +41,38 @@ int main()
     sum = crew.sumCrewHourlyRates();
     std::cout << "\nCrew's hourly pay: " << sum << std::endl;
     
-    crew.removeCrewMember(b);
+    crew.removeCrewMember(b);
     crew.listCrewMembers();
     sum = crew.sumCrewHourlyRates();
     std::cout << "\nCrew's hourly pay: " << sum << std::endl;
     */
+
+	fstream file;
+	string line,name;
+	file.open("default_config.txt");
+	if (file.is_open()){
+		while ( getline (file,line) )
+		{
+			name = line;
+			cout <<	name << ", is that you?" <<
+				endl<< "(y/n) ";
+				
+			string answer;
+			cin >> answer;
+			if (answer.compare("y") != 0)
+			{
+				cout << "Please enter your name." <<
+					endl<< ">> ";
+					
+				cin >> name;
+			}
+		}	
+	}
+	
+    cout << "Hello, " << name << endl <<
+    	"Welcome to the Delivery Profit Analysis System.\n\n";
     Delivery d1;
 //    d1.setEstimatedSetupTime(30);
     std::cout << "For this delivery, there was a profit of: " << d1.calculateDeliveryProfit() << std::endl;
-
+	file.close();
 }
